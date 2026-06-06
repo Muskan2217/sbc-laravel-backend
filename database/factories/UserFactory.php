44c44b0b039d12a9c403f03910451f5ac9bebc21
Muskan2217 <musk2217@gmail.com>
2026-06-07 01:15:20 +0530

    fix: correctly use local faker variable without this

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,13 +22,13 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+   public function definition(): array
     {
+        $faker = \Faker\Factory::create();
+
         return [
-            // 'name' => fake()->name(),
-            // 'email' => fake()->unique()->safeEmail(),
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(), 
+            'email' => $faker->unique()->safeEmail(), 
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
